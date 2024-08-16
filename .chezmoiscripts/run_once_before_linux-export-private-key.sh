f356d35ee98b97b3726da3148d287a2fd92ef450
Felipe Rios <felipe.rios.silva@outlook.com>
2024-08-15 21:36:55 -0400

    feat(pem): added feature to export private active PEM files
    Signed-off-by: Felipe Rios <felipe.rios.silva@outlook.com>

--- .chezmoiscripts/run_once_before_linux-export-private-key.sh
+++ .chezmoiscripts/run_once_before_linux-export-private-key.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # define the path to the .ssh folder
-sshFolderPath="$HOME\.ssh"
+sshFolderPath="$HOME/.ssh"
 
 # ensure the ".ssh" folder exists
 mkdir -p $sshFolderPath
 
 if [[ ! -f "$sshFolderPath/chezmoi" ]]; then
-    echo "Creating Chezmoi encryption key at: \"$sshFolderPath\chezmoi\"..."
+    echo "Creating Chezmoi encryption key at: \"$sshFolderPath/chezmoi\"..."
     op read "op://personal/Chezmoi Key/private key" > "$sshFolderPath/chezmoi"
     chmod 600 "$sshFolderPath/chezmoi"
 fi
--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ### TODO:
 - `know_hosts` are with binary content when executed via Chezmoi
 - manage the script files imported in the `.zshrc` inside another custom folder
-- export all active PEMs to the `.ssh` folder
 - export all active GPGs to the `.gpg` folder + import them in the keyring
 - check how to avoid 1Password duplicated calls
 - check how to use variables from a included template
