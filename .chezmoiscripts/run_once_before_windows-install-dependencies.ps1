6cebd6bee9cb013cc24ec95c6e608e8d6226e611
Felipe Rios <felipe.rios.silva@outlook.com>
2024-12-09 10:31:18 -0500

    hotfix(windows): added Windows binaries to install in all the OS
    Signed-off-by: Felipe Rios <felipe.rios.silva@outlook.com>

---
+++ .chezmoiscripts/run_once_before_windows-install-dependencies.ps1
@@ -0,0 +1,42 @@
+# Drivers and Tools
+winget install Asus.ArmouryCrate
+winget install TechPowerUp.GPU-Z
+winget install CPUID.CPU-Z
+winget install Asus.GPUTweak
+winget install Nvidia.GeForceExperience
+winget install Logitech.GHUB
+
+# Utilities
+#winget install Microsoft.PowerToys
+winget install Notepad++.Notepad++
+winget install VideoLAN.VLC
+#winget install ShareX.ShareX
+winget install Discord.Discord
+winget install SlackTechnologies.Slack
+winget install Spotify.Spotify
+winget install Valve.Steam
+#winget install Audacity.Audacity
+winget install GIMP.GIMP
+winget install Brother.FullDriver
+winget install CyberPowerSystems.PowerPanel.Personal
+
+# Development
+winget install JetBrains.Toolbox
+winget install GoLang.Go
+winget install Microsoft.VisualStudio.2022.Community
+winget install Docker.DockerDesktop
+winget install Git.Git
+winget install OpenVPNTechnologies.OpenVPNConnect
+winget install Mirantis.Lens
+winget install Microsoft.PowerShell
+winget install Microsoft.AzureStorageExplorer
+winget install Postman.Postman
+#winget install Cloudflare.Warp
+
+# Gaming
+winget install Valve.Steam
+winget install Blizzard.BattleNet
+winget install EpicGames.EpicGamesLauncher
+winget install ElectronicArts.EADesktop
+winget install GOG.Galaxy
+winget install Ubisoft.Connect
