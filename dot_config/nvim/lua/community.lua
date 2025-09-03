d4f97bd5d84442eb0889f51038db198aef8ea8a6
rios0rios0 <rios0rios0@outlook.com>
2025-09-02 22:43:35 -0300

    hotfix(android): removed NeoVIM not necessary configuration files

--- dot_config/nvim/lua/community.lua
+++
@@ -1,7 +0,0 @@
--- Minimal community plugins for language support
-return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.typescript" },
-}
